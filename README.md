3aaaea984af59759bf7eb85fdafdae3735f68286
st0rmman <stormman0522@gmail.com>
2022-04-08 20:32:46 +0800

    添加一键脚本

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
  vinerProxy100
 
-Windows 直接下载运行  
+# Windows 
+
+直接下载运行  
 
 [点击下载](https://raw.githubusercontent.com/vinerProxy/vinerProxy/master/release/vinerProxy.exe)
 
-Linux手动安装
+
+# linux
 
 
-# linux服务器
+## Linux自动安装
+bash <(curl -s -L https://raw.githubusercontent.com/vinerProxy/vinerProxy/master/scripts/onkey.sh)
+
+## Linux手动安装
+
 mkdir vinerproxy
 
 cd vinerproxy
@@ -18,11 +25,12 @@ chmod 777 vinerProxyv100_linux_amd64
 
 ./vinerProxyv100_linux_amd64
 
+## 运行状态
+### 查看 vinerProxy 运行
+screen -r vinerProxy
 
-查看 vinerProxy 运行
-
-退出查看 vinerProxy
-
+### 退出查看 vinerProxy
+CTRL+a+d
 
 TG交流群：https://t.me/+LUAZPPo3LYpiYjEx
 
---
+++ scripts/onkey.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+[[ $(id -u) != 0 ]] && echo -e "使用su命令切换到root用户再运行" && exit 1
+
+cmd="apt-get"
+if [[ $(command -v apt-get) || $(command -v yum) ]] && [[ $(command -v systemctl) ]]; then
+    if [[ $(command -v yum) ]]; then
+        cmd="yum"
+    fi
+else
+    echo "不支持此系统" && exit 1
+fi
+
+install() {
+    if [ -d "/root/vinerProxy" ]; then
+        echo -e "您已下载了vinerProxy，重新运行此脚本，并选2.卸载->1.安装" && exit 1
+    fi
+    if screen -list | grep -q "vinerProxy"; then
+        echo -e "vinerProxy已在运行中，请选6.停止->2.卸载->1.安装" && exit 1
+    fi
+
+    $cmd update -y
+    $cmd install curl wget screen -y
+    mkdir /root/vinerProxy
+	chmod 777 /root/vinerProxy
+
+    wget https://raw.githubusercontent.com/vinerproxy/VinerProxy/master/release/vinerProxyv100_linux_amd64 -O /root/vinerProxy/vinerProxy
+	
+	
+    chmod 777 /root/vinerProxy/vinerProxy
+
+    start
+}
+
+uninstall() {
+    read -p "是否确认删除vinerProxy[yes/no]：" flag
+    if [ -z $flag ]; then
+        echo "输入错误" && exit 1
+    else
+        if [ "$flag" = "yes" -o "$flag" = "ye" -o "$flag" = "y" ]; then
+            screen -X -S vinerProxy quit
+            rm -rf /root/vinerProxy
+            echo "卸载vinerProxy成功"
+        fi
+    fi
+}
+
+update() {
+    stop
+	uninstall
+	install
+	start
+}
+
+start() {
+    if screen -list | grep -q "vinerProxy"; then
+        echo -e "vinerProxy已启动" && exit 1
+    fi
+    echo "正在启动..."
+    screen -dmS vinerProxy
+    sleep 0.2s
+    screen -r vinerProxy -p 0 -X stuff "cd /root/vinerProxy"
+    screen -r vinerProxy -p 0 -X stuff $'\n'
+    screen -r vinerProxy -p 0 -X stuff "./vinerProxy"
+    screen -r vinerProxy -p 0 -X stuff $'\n'
+    sleep 5s
+    cat /root/vinerProxy/configV6.yml
+    echo "已启动web后台 您可: screen -r vinerProxy 查看程序输出;CTRL+A+D退出screen"
+}
+
+restart() {
+    stop
+    start
+}
+
+stop() {
+    if screen -list | grep -q "vinerProxy"; then
+        screen -X -S vinerProxy quit
+    fi
+    echo "vinerProxy 已停止"
+}
+
+change_limit(){
+    echo -n "当前连接数限制："
+    num="n"
+    if [ $(grep -c "root soft nofile" /etc/security/limits.conf) -eq '0' ]; then
+        echo "root soft nofile 102400" >>/etc/security/limits.conf
+        num="y"
+    fi
+
+    if [[ "$num" = "y" ]]; then
+        echo "连接数限制已修改为102400,重启服务器后生效"
+    else
+        echo -n "当前连接数限制："
+        ulimit -n
+    fi
+}
+
+
+echo "======================================================="
+echo "vinerProxy 一键工具"
+echo "  1、安装(默认安装到/root/vinerProxy)"
+echo "  2、卸载"
+echo "  3、更新"
+echo "  4、启动"
+echo "  5、重启"
+echo "  6、停止"
+echo "  7、解除连接数限制"
+echo "======================================================="
+read -p "$(echo -e "请选择[1-8]：")" choose
+case $choose in
+1)
+    install
+    ;;
+2)
+    uninstall
+    ;;
+3)
+    update
+    ;;
+4)
+    start
+    ;;
+5)
+    restart
+    ;;
+6)
+    stop
+    ;;
+7)
+    change_limit
+    ;;
+*)
+    echo "输入错误请重新输入！"
+    ;;
+esac
